7f51b2aa7ad781dc6567619f8aaa77ae3c03dd96
Charan Ganesh <charan.ganesh1999@gmail.com>
2024-10-31 16:18:33 +0530

    Initial Commit

---
+++ TMDbApp/TMDbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDbAppApp.swift
+//  TMDbApp
+//
+//  Created by Charan Ganesh on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TMDbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
